e80e18fc0907d67fd1a5b345c257086fe0f28209
Samridhi Mishra <samridhi.mishra0708@gmail.com>
2026-04-27 17:38:10 +0530

    Add attendance marking and viewing functions

--- app.py
+++ app.py
@@ -1,3 +1,12 @@
-print("✅ CI/CD Pipeline Running Successfully!")
-print("Clean code")
-print("Testing security")
+def mark_attendance(name):
+    with open("attendance.txt", "a") as file:
+        file.write(name + " Present\n")
+    print(f"{name} marked present")
+
+def view_attendance():
+    with open("attendance.txt", "r") as file:
+        print(file.read())
+
+# Demo
+mark_attendance("Samridhi")
+view_attendance()
